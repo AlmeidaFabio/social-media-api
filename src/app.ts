810677745bcb28b6f09d006af3f6eaf2db62fb36
almeidafabio <fabio.633@hotmail.com>
2024-01-05 09:10:47 -0300

    add errorHandler middleware

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { json, urlencoded } from 'express'
 import cors from 'cors'
 import path from 'path'
 import userRoutes from './routes/user'
+import { errorHandler } from './middlewares/ErrorHandler'
 
 const app = express()
 
@@ -9,6 +10,7 @@ app.use(cors())
 app.use(json())
 app.use(urlencoded({extended:true}))
 app.use(express.static(path.resolve(__dirname, '..', 'public')))
-app.use(userRoutes)
+app.use('/user', userRoutes)
+app.use(errorHandler)
 
 export { app }
